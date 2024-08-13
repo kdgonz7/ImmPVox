4c12c7af4f0ea477d5fde4a710197f49578bb91c
Kai D. Gonzalez <gkai70263@gmail.com>
2024-08-13 14:50:42 -0400

    make this test hook non-interfered

--- lua/pvox_module/pvox_testhooks.lua
+++ lua/pvox_module/pvox_testhooks.lua
@@ -6,5 +6,7 @@ if SERVER then
     hook.Add("PVOX_EmitAction", "PVOXEmitActionOverride", function(player, action, override, _time)
         -- actions can be ran multiple times a second, however they are throttled by
         -- the PVox system. So do take that into account when running these hooks.
+
+        return true
     end)
 end
