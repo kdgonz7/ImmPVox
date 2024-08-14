01d01619373d38cfda4e851a67754c591cea6686
Kai D. Gonzalez <gkai70263@gmail.com>
2024-08-14 00:28:12 -0400

    add the new options to spawnmenu

--- lua/autorun/client/playervox_spawnmenu.lua
+++ lua/autorun/client/playervox_spawnmenu.lua
@@ -3,6 +3,8 @@
 	end )
 
 	hook.Add( "PopulateToolMenu", "Cat232", function()
+		---
+		---@param panel DForm
 		spawnmenu.AddToolMenuOption( "Options", "PVOX", "PVOXSettings", "#PVOX Settings", "", "", function( panel )
 			panel:ClearControls()
 
@@ -19,11 +21,20 @@
 				panel:CheckBox("Specify Entity on killed and damage calls", "pvox_specifyotherentity")
 				panel:ControlHelp( "(ONLY WORKS FOR SUPPORTED MODULES) Enables specific entity voice lines. e.g. instead of enemy_killed it will use e.g. soldier_killed, antlion_killed, etc." )
 
+				panel:CheckBox("Only call kill-confirms once", "pvox_senddamageonce")
+				panel:ControlHelp( "Throttles shooting dead bodies to having it call out only once." )
+
+				panel:CheckBox("Localization Language", "pvox_localizationlang")
+				panel:ControlHelp( "The GLOBAL language for PVOX CC. This IS A SERVER SIDE VALUE. Defaults to en-US, and different languages can depend on how the module implements them." )
+
+				panel:CheckBox("Enable CC (Closed Captions)", "pvox_useclosedcaptioning")
+				panel:ControlHelp( "Enables printing audio files to the chat, from a player, as they're being played. Similar to Counter-Strike games." )
+
 				panel:CheckBox( "Allow Notes", "pvox_allownotes" )
 				panel:CheckBox( "Suppress Warnings", "pvox_suppresswarnings" )
 			end
 
-
+			---@class DComboBox
 			local Combo = panel:ComboBox( "PVOX Preset", "pvox_mypreset" )
 			panel:ControlHelp( "Change your current PVOX preset. Will stop on respawn unless Use 'Player Model Binds' is off." )
 
