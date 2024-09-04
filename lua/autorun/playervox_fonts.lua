d90b77c43135bcccb2a239c28212303c0a524cb2
Kai D. Gonzalez <gkai70263@gmail.com>
2024-09-03 20:14:41 -0400

    use new font

--- lua/autorun/playervox_fonts.lua
+++ lua/autorun/playervox_fonts.lua
@@ -17,3 +17,20 @@ surface.CreateFont("PVox-Radial-HUD-Font", {
 	additive = false,
 	outline = false,
 })
+surface.CreateFont("PVox-Normal-HUD-Font", {
+	font = "Source Code Pro", -- Use the font-name which is shown to you by your operating system Font Viewer, not the file name
+	extended = false,
+	size = 25,
+	weight = 100,
+	blursize = 0,
+	scanlines = 0,
+	antialias = true,
+	underline = false,
+	italic = false,
+	strikeout = false,
+	symbol = false,
+	rotary = false,
+	shadow = false,
+	additive = false,
+	outline = false,
+})
